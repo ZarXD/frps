3454127dcaf16b4915273417ae429a6770c3b219
ZaaXyyZ <91553852+ZarXD@users.noreply.github.com>
2025-06-23 06:59:12 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ bind_port = 80
 dashboard_port = 7500
 dashboard_user = admin
 dashboard_pwd = admin
+tls_only = false
 EOF
 
 # Ini penting: jalanin frps sebagai foreground (jangan & atau background)
