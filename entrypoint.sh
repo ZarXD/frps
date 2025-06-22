b5cfa14f888a50c33ee56e420c9efa4555699dd4
ZaaXyyZ <91553852+ZarXD@users.noreply.github.com>
2025-06-23 06:47:14 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+
 cat <<EOF > frps.ini
 [common]
-bind_port = ${BIND_PORT:-7000}
-bind_udp_port = ${BIND_UDP_PORT:-7001}
-dashboard_port = ${DASHBOARD_PORT:-7500}
-dashboard_user = ${DASH_USER:-admin}
-dashboard_pwd = ${DASH_PWD:-admin}
+bind_port = 7000
+dashboard_port = 7500
+dashboard_user = admin
+dashboard_pwd = admin
 EOF
 
-/usr/local/bin/frps -c frps.ini
+# Ini penting: jalanin frps sebagai foreground (jangan & atau background)
+./frps -c frps.ini
